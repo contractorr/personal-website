d68efe7409e09d5c9260636027f601ae5184246f
contractorr <rajcontractor@hotmail.co.uk>
2020-12-16 13:48:57 +0000

    Adding front end

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// Open and close sidebar
+function toggleNav(x) {
+	if (document.getElementById("myNavPanel").style.width == "100%") {
+		document.getElementById("myNavPanel").style.width = "0%";
+	}	else {
+		document.getElementById("myNavPanel").style.width = "100%";
+	}
+	x.classList.toggle("change");	
+}
